cb161ddd928c59b632fc99f170bee1095e9991af
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-16 16:38:38 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double F, C;
+            Console.WriteLine("Tell me tempereture in Fahrenheit");
+            F = double.Parse(Console.ReadLine());
+            C = 5d / 9 * (F - 32);
+            Console.WriteLine(C);
+            Console.ReadKey();
+
+        }
+    }
+}
